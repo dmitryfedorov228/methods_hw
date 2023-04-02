03dfdbb798e49c174673c76dc7e675f2ae8bf689
dmitryfedorov228 <116007725+dmitryfedorov228@users.noreply.github.com>
2023-04-02 13:52:16 +0600

    add files

---
+++ main.js
@@ -0,0 +1,63 @@
+//! TASK1 ------------------------
+
+// const arr = [1, 2, 3] 
+// const mas = [4, 5, 6]
+// const arrMas = arr.concat(mas)
+// console.log(arrMas);
+
+//! TASK2 ------------------------
+
+// const arr = [1, 2, 3]
+// console.log(arr.reverse());
+
+//! TASK3 ------------------------
+
+// const mas = [1, 2, 3]
+// let total = mas.push(4, 5, 6)
+// console.log(mas);
+
+//! TASK4 ------------------------
+
+// const arr = [1, 2, 3]
+// arr.unshift(4, 5, 6)
+
+// console.log(arr);
+
+//! TASK5 ------------------------
+
+// const mas =  ['js', 'css', 'jq']
+// console.log(mas[0]);
+
+//! TASK6 ------------------------
+
+// let arr = [1, 2, 3, 4, 5]
+// console.log(arr.slice(0, 3));
+
+//! TASK7 ------------------------
+
+// const arr =  [1, 2, 3, 4, 5]
+// console.log(arr.slice(3, 5));
+
+//! TASK8 ------------------------
+
+// let mas = [1, 2, 3, 4, 5]
+// let splice = mas.splice(1, 2)
+// console.log(mas);
+
+//! TASK9 ------------------------
+
+// const arr =  [1, 2, 3, 4, 5]
+
+// console.log(arr.splice(1, 3));
+
+//! TASK10 ------------------------
+
+// const mas = [1, 2, 3, 4, 5]
+// let result = mas.splice(3, 0, 'a', 'b', 'c')
+// console.log(mas);
+
+//! TASK11 ------------------------
+
+// const arr = [1, 2, 3, 4, 5]
+// let result = arr.splice(1, 4,  'a', 'b', 2, 3, 4, 'c', 5, 'e')
+// console.log(arr);
